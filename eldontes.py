1dfc3884afd2999e9705907b2f0b9bd0d700422e
Domawas <dominknagy@gmail.com>
2025-01-28 10:13:49 +0100

    absolut ertek

--- eldontes.py
+++ eldontes.py
@@ -9,16 +9,36 @@ def tizenharom():
 
 tizenharom()
 
-def elso_tiz_paros():
+
+def elso_tiz_párosok_osszege():
     osszeg = 0
-    páros_számok = 0
-
-    for i in range(2, 21, 2):  
-        osszeg += i
-        páros_számok += 1
-        if páros_számok == 10:  
-            return osszeg
-
-print(f"Az első 10 páros szám összeg: {elso_tiz_paros()}")
+    for i in range(1, 11): 
+        if i % 2 == 0: 
+            osszeg += i
+    return osszeg
 
 
+print(f"Az első 10 szám párosainak összeg: {elso_tiz_párosok_osszege()}")
+
+
+def elso_38_paratlan():
+    osszeg = 0
+    szam = 24 
+    for _ in range(38):
+        osszeg += szam
+        szam += 2  
+    return osszeg
+
+
+print(f"Az első 38 páratlan szám összege 24-től kezdve: {elso_38_paratlan()}")
+
+
+def abszolut_ertek():
+    for i in range(3, -4, -1):
+        if i < 0:
+            print(f"|{i}| = {-i}")
+        else:
+            print(f"|{i}| = {i}")
+
+
+abszolut_ertek() 
